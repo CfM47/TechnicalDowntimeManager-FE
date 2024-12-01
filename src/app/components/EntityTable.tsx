495f3ec980f129b0a86c7b19c3c6891348f75ab5
CfM47 <jossuearteche000@gmail.com>
2024-12-01 13:22:39 -0500

    feat: refactor EntitiesPage and EntityTable components for improved structure and flexibility

--- src/app/components/EntityTable.tsx
+++ src/app/components/EntityTable.tsx
@@ -1,58 +1,23 @@
-'use client';
+import { ReactNode } from 'react';
 
-import { RowDropdownMenu } from './RowDropdownMenu';
+import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from '@/components/ui/table';
-
-export type TableHeader<T extends Record<string, any>> = {
-  key: keyof T;
-  label: string;
-};
-
-interface EntityTableProps<T extends Record<string, any>> {
-  headers: TableHeader<T>[];
-  data: Array<T>;
+interface EntityTableProps {
+  heads: Array<string>;
+  body: ReactNode;
 }
 
-export const EntityTable = <T extends Record<string, any>>({
-  data,
-  headers
-}: EntityTableProps<T>) => {
+export const EntityTable = ({ heads, body }: EntityTableProps) => {
   return (
     <Table>
       <TableHeader>
         <TableRow>
-          {headers.map((header, index) => (
-            <TableHead key={index}>{header.label}</TableHead>
+          {heads.map((head, index) => (
+            <TableHead key={index}>{head}</TableHead>
           ))}
-          <TableHead>Acciones </TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        {data.map((item, index) => (
-          <TableRow key={index}>
-            {headers.map((header, index) => (
-              <TableCell key={index}>{item[header.key]}</TableCell>
-            ))}
-            <TableCell>
-              <RowDropdownMenu
-                options={[
-                  { label: 'Editar', action: () => Promise.resolve() },
-                  { label: 'Eliminar', action: () => Promise.resolve() },
-                  { label: 'Detalles', action: () => Promise.resolve() }
-                ]}
-              />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
+      <TableBody>{body}</TableBody>
     </Table>
   );
 };
