020fc3e4493c5ba46ae4037b2e0d477c9affb190
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-12-24 10:14:11 -0500

    feat/types added

---
+++ src/types/transfer.ts
@@ -0,0 +1,9 @@
+export interface Transfer {
+  id_sender: string;
+  id_receiver: string;
+  id_equipment: string;
+  date: string;
+  id_origin_dep: string;
+  id_receiver_dep: string;
+  downtime_status: string;
+}
---
+++ src/types/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+  id: string;
+  name: string;
+  id_department: string;
+  id_role: number;
+}
