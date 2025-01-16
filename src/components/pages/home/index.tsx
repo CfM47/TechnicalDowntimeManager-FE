04b706348b286336ecfb1bb78c30b88adadeb470
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-01-16 11:50:52 -0500

    background for landing page added

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -5,7 +5,7 @@ interface HomePageProps {}
 
 export const HomePage = ({}: HomePageProps) => {
   return (
-    <div className="flex flex-col min-h-screen items-center">
+    <div className="flex flex-col min-h-screen items-center bg-cover bg-no-repeat bg-[url('/assets/back_sm.svg')] md:bg-[url('/assets/back.svg')]">
       <main className="flex-1">
         <Hero />
       </main>
