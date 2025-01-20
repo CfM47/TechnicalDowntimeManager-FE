f18c0c4f6e48d8d3455992bc7f94742815fc4f1e
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-01-20 15:51:41 -0500

    delete sign up option

--- src/components/modals/auth-modal/components/BodyContent.tsx
+++ src/components/modals/auth-modal/components/BodyContent.tsx
@@ -1,18 +1,13 @@
 import { TabListContainer } from '@/components/containers/tab-list-container';
 import { SignInForm } from '@/components/forms/sign-in';
-import { SignUpForm } from '@/components/forms/sign-up';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface BodyContentProps {}
 
 export const BodyContent = ({}: BodyContentProps) => {
-  const tabsDefinition = [
-    { value: 'signin', label: 'Iniciar sesión' },
-    { value: 'signup', label: 'Registrarse' }
-  ];
+  const tabsDefinition = [{ value: 'signin', label: 'Iniciar sesión' }];
   const tabsComponents = {
-    signin: <SignInForm />,
-    signup: <SignUpForm />
+    signin: <SignInForm />
   };
 
   return <TabListContainer {...{ tabsDefinition, tabsComponents }} />;
