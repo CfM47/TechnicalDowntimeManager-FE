fb7ae91166343f9d0110f75030a3b9ab9594db91
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-01-22 08:30:25 -0500

    added the same autentication logic to the responsive side menu

--- src/components/common/side-menu/index.tsx
+++ src/components/common/side-menu/index.tsx
@@ -1,20 +1,31 @@
 'use client';
 
-import { Menu } from 'lucide-react';
+import { LogOut, Menu } from 'lucide-react';
 import { useState } from 'react';
 
 import { SigninModal } from '../../modals/sign-in-modal';
 import { HighlightLink } from '../highlight-link';
 
+import { InitialsAvatar } from '@/components/common/initials-avatar';
+import { PrivateRouteContainer } from '@/components/containers/private-route-container';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import useSessionStore from '@/stores/sesionStore';
+import { useRouter } from 'next/navigation';
 
 interface SideMenuProps {
-  items: Array<{ name: string; href: string }>;
+  items: Array<{ name: string; href: string; authorizedRoles: number[] }>;
 }
 
 export const SideMenu = ({ items }: SideMenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const { clear, token, name } = useSessionStore();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    clear();
+    router.push('/');
+  };
 
   return (
     <div className="flex items-center">
@@ -28,14 +39,27 @@ export const SideMenu = ({ items }: SideMenuProps) => {
         <SheetContent side="right">
           <div className="flex flex-col space-y-4 mt-4">
             {items.map((item, index) => (
-              <HighlightLink
-                key={index}
-                {...item}
-                className="text-gray-500 font-semibold"
-                onClick={() => setIsOpen(false)}
-              />
+              <PrivateRouteContainer key={index} authorizedRoles={item.authorizedRoles}>
+                <HighlightLink
+                  {...item}
+                  className="text-gray-500 font-semibold"
+                  onClick={() => setIsOpen(false)}
+                />
+              </PrivateRouteContainer>
             ))}
-            <SigninModal />
+            {token ? (
+              <div className="flex items-center space-x-4">
+                <InitialsAvatar name={name ?? ': )'} />
+                <Button
+                  onClick={handleLogout}
+                  className=" bg-white text--black border-transparent rounded-full hover:text-red-600 hover:bg-white transition-colors"
+                >
+                  <LogOut />
+                </Button>
+              </div>
+            ) : (
+              <SigninModal />
+            )}
           </div>
         </SheetContent>
       </Sheet>
