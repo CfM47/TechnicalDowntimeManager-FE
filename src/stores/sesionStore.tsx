05576eb94304c032adce52e169f2ec3c902ef371
CfM47 <jossuearteche000@gmail.com>
2025-01-22 00:28:44 -0500

    store

--- src/stores/sesionStore.tsx
+++ src/stores/sesionStore.tsx
@@ -1,17 +1,38 @@
+'use client';
+
 import { create } from 'zustand';
 
 interface SessionState {
+  name: string | null;
   token: string | null;
-  role: string;
+  role: string | null;
+  setName: (name: string) => void;
   setToken: (token: string) => void;
   setRole: (role: string) => void;
 }
 
 const useSessionStore = create<SessionState>()((set) => ({
+  name: localStorage.getItem('name'),
   token: localStorage.getItem('token'),
-  role: '',
-  setToken: (token: string) => set({ token }),
-  setRole: (role: string) => set({ role })
+  role: localStorage.getItem('role'),
+  setName: (name: string) => {
+    localStorage.setItem('name', name);
+    set({ name });
+  },
+  setToken: (token: string) => {
+    localStorage.setItem('token', token);
+    set({ token });
+  },
+  setRole: (role: string) => {
+    localStorage.setItem('role', role);
+    set({ role });
+  },
+  clear: () => {
+    localStorage.removeItem('name');
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    set({ name: null, token: null, role: null });
+  }
 }));
 
 export default useSessionStore;
