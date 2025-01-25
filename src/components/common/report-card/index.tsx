17144e6fc9e01e52c377a81105a2e36498cc0955
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2025-01-25 18:03:07 -0500

    create reports page

---
+++ src/components/common/report-card/index.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+
+/**
+ * @interface ReportCardProps
+ * @property {string} title - El título del reporte.
+ * @property {string} description - La descripción del reporte.
+ * @property {() => void} onGenerate - Función que se ejecuta al hacer clic en el botón "Generar".
+ */
+interface ReportCardProps {
+  title: string;
+  description: string;
+  onGenerate?: () => void;
+}
+
+/**
+ * Componente `ReportCard` que muestra una tarjeta con un título, descripción y un botón para generar un reporte.
+ * @param {ReportCardProps} props - Las propiedades del componente `ReportCard`.
+ * @param {string} props.title - El título del reporte.
+ * @param {string} props.description - La descripción del reporte.
+ * @param {() => void} props.onGenerate - Función que se ejecuta al hacer clic en el botón "Generar".
+ * @returns {JSX.Element} - El componente `ReportCard`.
+ */
+export const ReportCard = ({ title, description, onGenerate }: ReportCardProps) => {
+  return (
+    <Card className="w-full max-w mx-auto rounded-lg shadow-lg bg-white p-2">
+      <CardContent>
+        <h2 className="text-xl font-bold text-gray-800 mb-4">{title}</h2>
+        <p className="text-gray-600 text-base mb-6">{description}</p>
+        <div className="flex justify-end ">
+          <Button
+            className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-600"
+            onClick={onGenerate}
+          >
+            Generar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
